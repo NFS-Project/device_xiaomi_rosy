238d6a6520439739ad916bbccc8f0eccfc301ec5
Mathieu Chartier <Mathieu Chartier@users.noreply.github.com>
2022-11-11 21:28:19 +0530

    rosy: Always preopt extracted APKs
    Change-Id: I2e4742a796b8142f14eaad5eba81eaf800d64914

--- device.mk
+++ device.mk
@@ -419,6 +419,10 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so
 
+# Always preopt extracted APKs to prevent extracting out of the APK
+# for gms modules.
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
